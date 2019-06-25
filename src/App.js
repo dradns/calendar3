cb16f5946e92f1783b35f74790cf517b79ca6006
OUT-Savin-NL <savin.n@hotmail.com>
2019-06-25 10:24:08 +0300

    vt2

--- src/App.js
+++ src/App.js
@@ -7,143 +7,93 @@ import 'semantic-ui-css/semantic.min.css';
 
 
 let curDate = DateTime.local();
-//////////
-//старт///
-//////////
+
 const App = () => (
     <Grid columns={1} centered>
       <Grid.Row>
-          <Head/>
+          <Grid celled>
+              <Grid.Row >
+                  <Grid.Column width={4}>
+                      <Grid style={{ justifyContent: 'space-evenly'}}>
+                          <Grid.Column>
+                              <h1>{curDate.day}</h1>
+                          </Grid.Column>
+
+                          <Grid.Column>
+                              <h1>{monthName()}</h1>
+                          </Grid.Column>
+
+                          <Grid.Column>
+                              <h1>{curDate.year}</h1>
+                          </Grid.Column>
+                      </Grid>
+                  </Grid.Column>
+
+                  <Grid.Column width={8}>
+                      <Button.Group fluid>
+                          <Button href='/day'>День</Button>
+                          <Button href='/week'>Неделя</Button>
+                          <Button href='/month'>Месяц</Button>
+                          <Button href='/year'>Год</Button>
+                      </Button.Group>
+                  </Grid.Column>
+
+                  <Grid.Column width={4}>
+                      <Grid style={{ justifyContent: 'space-evenly'}}>
+                          <Grid.Row>
+                              <Button icon='angle double left' onClick={() => { alert('hey') }}/>
+                              <Segment color='grey' content='today'>{}</Segment>
+                              <Button icon='angle double right' onClick={changeMonth}/>
+                          </Grid.Row>
+                      </Grid>
+                  </Grid.Column>
+              </Grid.Row>
+          </Grid>
       </Grid.Row>
 
       <Grid.Row>
-        <Body />
+          <Grid>
+              <Grid.Row>
+                  <Grid.Column width={16}>
+                      <Grid columns={7}>
+                          <Grid.Row>
+                              { _.times(7, i => (
+                                        <Grid.Column key={i} >
+                                        <Segment color='orange' textAlign='center'>{dayWeek(i)}</Segment>
+                                        </Grid.Column>))
+                              }
+                          </Grid.Row>
+                      </Grid>
+                  </Grid.Column>
+              </Grid.Row>
+
+              <Grid.Row>
+                  <Grid.Column width={16}>
+                      <React.Fragment>
+                          {/*<Route  path={'/day'} component={BodyGridDay} />*/}
+                          {/*<Route  path={'/week'} component={BodyGridWeek} />*/}
+                          <Route  path={'/month'} component={BodyGridMonth} />
+                          {/*<Route  path={'/year'} component={BodyGridYear} />*/}
+                      </React.Fragment>
+                  </Grid.Column>
+              </Grid.Row>
+          </Grid>
       </Grid.Row>
     </Grid>
 );
-
-//////////
-//head////
-//////////
-const Head = () => (
-    <Grid celled>
-      <Grid.Row >
-        <Grid.Column width={4}>
-          <CurrentCorner />
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <ButtonPanel />
-        </Grid.Column>
-
-        <Grid.Column width={4}>
-          <Switcher />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-);
-
-/////////
-//body///
-/////////
-const Body = () => (
-    <Grid>
-      <Grid.Row>
-        <Grid.Column width={16}>
-          <BodyBar />
-        </Grid.Column>
-      </Grid.Row>
-
-      <Grid.Row>
-        <Grid.Column width={16}>
-          <BodyRouterGrid />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-);
-
-//////////////////
-//currentCorner///
-//////////////////
-const CurrentCorner = () => (
-    <Grid style={{ justifyContent: 'space-evenly'}}>
-      <Grid.Column>
-        <h1>{curDate.day}</h1>
-      </Grid.Column>
-
-      <Grid.Column>
-        <h1>{monthName()}</h1>
-      </Grid.Column>
-
-      <Grid.Column>
-        <h1>{curDate.year}</h1>
-      </Grid.Column>
-    </Grid>
-);
-
-///////////////
-//buttonPanel//
-///////////////
-const ButtonPanel = () => (
-    <Button.Group fluid>
-      <Button href='/day'>День</Button>
-      <Button href='/week'>Неделя</Button>
-      <Button href='/month'>Месяц</Button>
-      <Button href='/year'>Год</Button>
-    </Button.Group>
-);
-
-const Dates = () => _.times(curDate.daysInMonth, i => (
-    <Grid.Column key={i} >
-      <Button basic color='teal' size='mini' fluid style={{marginTop: '10px'}} onClick={createEvent}>
-        {dayMonth(i)+ ' day'}
-      </Button>
-    </Grid.Column>
-));
 
 const BodyGridMonth = () => (
     <Grid columns={7} >
       <Grid.Row style={{marginTop: '20px'}}>
-        <Dates />
+          { _.times(curDate.daysInMonth, i => (
+              <Grid.Column key={i} >
+              <Button basic color='teal' size='mini' fluid style={{marginTop: '10px'}} onClick={createEvent}>
+              {dayMonth(i)+ ' day'}
+              </Button>
+              </Grid.Column>
+              ))}
       </Grid.Row>
     </Grid>
-);
-
-//////////////
-//switcher////
-//////////////
-const Switcher = () => (
-        <Grid style={{ justifyContent: 'space-evenly'}}>
-          <Grid.Row>
-            <Button icon='angle double left' onClick={() => { alert('hey') }}/>
-            <Segment color='grey' content='today'>{}</Segment>
-            <Button icon='angle double right' onClick={changeMonth}/>
-          </Grid.Row>
-        </Grid>
-);
-
-////////////////
-//bodyBar///////
-////////////////
-
-const BodyBar = () => (
-    <Grid columns={7}>
-      <Grid.Row>
-        <Weekdays />
-      </Grid.Row>
-    </Grid>
-);
-
-//////////////////
-//BodyRouterGrid//
-//////////////////
-const BodyRouterGrid = () => (
-    <React.Fragment>
-      {/*<Route  path={'/day'} component={BodyGridDay} />*/}
-      {/*<Route  path={'/week'} component={BodyGridWeek} />*/}
-      <Route  path={'/month'} component={BodyGridMonth} />
-      {/*<Route  path={'/year'} component={BodyGridYear} />*/}
-    </React.Fragment>
 );
 
 function monthName() {
@@ -169,14 +119,9 @@ function dayWeek(i) {
     return mas[i];
 }
 
-const Weekdays = () => _.times(7, i => (
-    <Grid.Column key={i} >
-        <Segment color='orange' textAlign='center'>{dayWeek(i)}</Segment>
-    </Grid.Column>
-));
-
 const changeMonth = () => {
-    alert('some');
+    curDate = curDate.minus({month: 1});
+    alert(curDate);
 };
 
 export default App;
